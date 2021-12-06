4ab4aaf7cf12a91f892d6d471f9a189873fe8d2f
jotammendes <jpaulo.mmendes@gmail.com>
2021-12-06 14:12:28 -0300

    feature: implementação de banner da página Home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
+  <Banner />
   <div class="main-container">
-    <h1>HOME</h1>
+    <h1>Monte o seu burger:</h1>
+    <p>Formulário</p>
   </div>
 </template>
 
 <script>
+  import Banner from '../components/Banner';
+
   export default {
     name: 'Home',
+    components: {
+      Banner
+    }
   }
 </script>
