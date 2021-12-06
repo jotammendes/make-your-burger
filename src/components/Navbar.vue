1ee4ba3346bfed3504cff29f9d6e9446ce09e86d
jotammendes <jpaulo.mmendes@gmail.com>
2021-12-06 14:01:26 -0300

    style: estilização de Navbar e Footer

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,51 @@
 <template>
   <div id="nav">
-    <router-link to="/">
-      <img src="" alt="Logo">
+    <router-link to="/" id="logo_url">
+      <img id="logo" :src="logo" :alt="alt">
     </router-link>
-    <router-link to="/">Home</router-link> |
+    <router-link to="/">Home</router-link>
     <router-link to="/order">Pedidos</router-link>
   </div>
 </template>
 
 <script>
   export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    props: {
+      logo: String,
+      alt: String
+    }
   }
 </script>
 
 <style scoped>
+  #nav {
+    background-color: #222;
+    border-bottom: 4px solid #111;
+    padding: 15px 50px;
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+  }
 
+  #nav #logo_url {
+    margin: auto;
+    margin-left: 0;
+  }
+
+  #logo {
+    width: 40px;
+    height: 40px;
+  }
+
+  #nav a {
+    color: #FCBA03;
+    text-decoration: none;
+    margin: 12px;
+    transition: 0.5s;
+  }
+
+  #nav a:hover {
+    color: #fff;
+  }
 </style>
